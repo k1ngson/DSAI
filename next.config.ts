e56f525f0c927b8613187a7af0701a3d91d1eae0
ON KING FOR <kingsleyon1206@gmail.com>
2026-01-29 01:40:04 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'https://dsaibackend.onrender.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
