18572e65bd1763f919c43bab6256d5c682376271
ON KING FOR <kingsleyon1206@gmail.com>
2026-02-11 17:21:16 +0800

    .

--- app/(protected)/settings/SettingsClient.tsx
+++ app/(protected)/settings/SettingsClient.tsx
@@ -8,8 +8,8 @@ import { supabase } from "../../../src/utils/supabaseClient";
 type Theme = "dark" | "light";
 
 const TOKEN_LIMITS = {
-  student: 20000,
-  teacher: 40000,
+  student: 40000,
+  teacher: 100000,
   admin: 150000
 };
 
